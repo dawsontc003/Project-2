c9452f017fada0194ee9a0e7562ca8083154bdcd
Sam Fan <samisfan@msn.com>
2021-01-13 12:40:46 -0700

    got server connected to dbname, need to rename db when complete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,45 @@
+// still need db table names,
+// id, ans1, ans2, ans3, ans4, ans5, userName, points, animalImage, animalData
+
 // Requiring our models
 const db = require("../models");
 
 // Routes
 module.exports = (app) => {
-  // GET route for sql data
-  app.get("/", (req, res) => {});
+  // GET route for recent results
+  app.get("/", (req, res) => {
+    // findAll returns all entries for a table when used with no options
+    db.Results.findAll({}).then((data) => {
+      // We have access to the todos as an argument inside of the callback function
+      res.json(data);
+    });
+  });
 
-  // POST
-  app.post("/api/todos", (req, res) => {});
+  // POST route for questions
+  app.post("/api/questions", (req, res) => {});
 
-  app.delete("/api/todos/:id", (req, res) => {});
+  // GET route for result
+  app.get("/api/result/:id", (req, res) => {});
 
-  // PUT route
-  app.put("/api/todos", (req, res) => {});
+  // PUT route - update
+  app.put("/api/revise", (req, res) => {
+    // Use the sequelize update method to update quiz answer
+    db.Revise.update(
+      { text: req.body.text, complete: req.body.complete },
+      { where: { id: req.body.id } }
+    ).then((data) => res.json(data));
+  });
+
+  // GET route for recent results
+  app.get("/api/recents", (req, res) => {
+    // findAll returns all entries for a table when used with no options
+    db.Recents.findAll({}).then((data) => {
+      // We have access to the todos as an argument inside of the callback function
+      res.json(data);
+    });
+  });
+
+  // We need a delete
+
+  app.delete("/api/recents/:id", (req, res) => {});
 };
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS dbname;
+-- Creates the "dbname" database, need updated name from Thomas--
+CREATE DATABASE dbname;
