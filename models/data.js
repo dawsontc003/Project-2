d84a2d333b143fdae00891322682edbf5a006f6b
Thomas Dawson <dawsontc003@gmail.com>
2021-01-14 18:47:07 -0700

    corrected data model to leverage integers

--- models/data.js
+++ models/data.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const animaldb = sequelize.define("animal_score", {
     name: DataTypes.STRING,
-    q_1: DataTypes.STRING,
-    q_2: DataTypes.STRING,
-    q_3: DataTypes.STRING,
+    q_1: DataTypes.INTEGER,
+    q_2: DataTypes.INTEGER,
+    q_3: DataTypes.INTEGER,
   });
   return animaldb;
 };
--- public/js/view.js
+++ public/js/view.js
@@ -1,3 +1,4 @@
 // Wait for the DOM to completely load before we run our JS
-document.addEventListener('DOMContentLoaded', (e) => {
-  console.log('dom loaded!');
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("dom loaded!");
+});
