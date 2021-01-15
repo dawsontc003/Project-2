b75f0b4b22c1a9f16ca12e14e8b28eb632b300ef
Thomas Dawson <dawsontc003@gmail.com>
2021-01-14 19:38:02 -0700

    working connection for get route

--- models/Animaldb.js
+++ models/Animaldb.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
-  const animaldb = sequelize.define("animal_score", {
+  const Animaldb = sequelize.define("Animaldb", {
     name: DataTypes.STRING,
     q_1: DataTypes.INTEGER,
     q_2: DataTypes.INTEGER,
     q_3: DataTypes.INTEGER,
   });
-  return animaldb;
+  return Animaldb;
 };
